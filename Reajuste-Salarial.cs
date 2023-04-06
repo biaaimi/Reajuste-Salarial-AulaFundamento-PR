3ca4d350e23552bd194fedb393d49705338b21fc
Beatriz Aimi <biagioni.aimi@gmail.com>
2023-04-06 10:12:59 -0300

    Add files via upload

---
+++ Reajuste-Salarial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReajusteSalarial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Double Salario;
+
+            Console.WriteLine("informe seu salário: ");
+            Console.WriteLine();
+            Console.WriteLine("Salario: ");
+
+            double.TryParse(Console.ReadLine(), out Salario);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Salário Atual: ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(Salario.ToString());
+
+            Console.ResetColor();
+            Console.WriteLine();
+
+            Console.WriteLine("Novo salário: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.Write(p.CalculaSalario(Salario));
+
+            Console.ReadKey();
+        }
+        private double CalculaSalario(double Salario)
+        {
+            double NovoSalario = 0;
+
+            if (Salario >= 1700) {
+                NovoSalario = (Salario + 200);
+            }
+            else
+            {
+                NovoSalario = (Salario + 300);
+            }
+
+            return NovoSalario;
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    
+
